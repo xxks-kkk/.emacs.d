03c9b34aa93c62b243e5d6b055f7a9343599eb50
Zeyuan On Hadoop <ferrishu3886@gmail.com>
2017-01-02 15:26:42 +0800

    Update init.el

--- init.el
+++ init.el
@@ -108,6 +108,33 @@
 (defun auto-complete-mode-maybe ()
  (auto-complete-mode 1))
 
+;; Makes *scratch* empty.
+(setq initial-scratch-message "")
+
+;; Removes *scratch* from buffer after the mode has been set.
+(defun remove-scratch-buffer ()
+  (if (get-buffer "*scratch*")
+      (kill-buffer "*scratch*")))
+(add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
+
+;; Removes *messages* from the buffer.
+;;(setq-default message-log-max nil)
+;;(kill-buffer "*Messages*")
+
+;; Removes *Completions* from buffer after you've opened a file.
+(add-hook 'minibuffer-exit-hook
+          '(lambda ()
+             (let ((buffer "*Completions*"))
+               (and (get-buffer buffer)
+                    (kill-buffer buffer)))))
+
+;; Don't show *Buffer list* when opening multiple files at the same time.
+(setq inhibit-startup-buffer-menu t)
+
+;; Show only one active window when opening multiple files at the same time.
+(add-hook 'window-setup-hook 'delete-other-windows)
+
+
 ;;------------------------------------
 ;; Key bindings
 ;;------------------------------------
@@ -137,6 +164,8 @@
 (defalias 'rof 'recentf-open-files) ; list recently opened files
 (defalias 'cy 'clipboard-yank); copy the text from clipboard
 (defalias 'ck 'clipboard-kill-region); copy the text to clipboard
+(defalias 'cr 'comment-region);
+(defalias 'ucr 'uncomment-region);
 
 ;;----------------------------------
 ;; Global modes setting
