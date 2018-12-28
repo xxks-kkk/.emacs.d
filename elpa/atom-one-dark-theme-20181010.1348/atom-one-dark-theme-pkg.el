c159e6a34e698a5166e684c4404b75ad2df42ea5
xxks-kkk <ferrishu3886@gmail.com>
2018-12-28 00:21:14 -0600

    Clean up ignored files

--- elpa/atom-one-dark-theme-20181010.1348/atom-one-dark-theme-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "atom-one-dark-theme" "20181010.1348" "Atom One Dark color theme" 'nil :commit "8cba629dce7a2c53c690337915fdcaa80b4af769" :authors '(("Jonathan Chu" . "me@jonathanchu.is")) :maintainer '("Jonathan Chu" . "me@jonathanchu.is") :url "https://github.com/jonathanchu/atom-one-dark-theme")
