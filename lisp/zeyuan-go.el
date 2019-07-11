3ec5368953e28f84e71e8ca753be23095c5abe5e
xxks-kkk <ferrishu3886@gmail.com>
2019-07-11 16:29:57 -0500

    Update go setup

--- lisp/zeyuan-go.el
+++ lisp/zeyuan-go.el
@@ -3,6 +3,7 @@
 ;;----------------------------------
 
 (require 'go-mode)
+(define-key go-mode-map (kbd "<f5>") 'godef-jump-other-window)
 (add-hook 'before-save-hook #'gofmt-before-save)
 
 (provide 'zeyuan-go)
