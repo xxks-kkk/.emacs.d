e10c07465c1526270db2b41982de156f030dd8bc
xxks-kkk <ferrishu3886@gmail.com>
2019-07-23 11:32:23 -0500

    Add rg configuration

--- lisp/setup-helm.el
+++ lisp/setup-helm.el
@@ -73,8 +73,11 @@
       "ggrep --color=always -d recurse %e -n%cH -e %p %f"))
 
     ; use Ag (sliver-search) keybind: M-g a in helm buffer
-    (setq helm-grep-ag-command "ag --line-numbers -S --hidden --color --color-match '31;43' --nogroup %s %s %s")
-    (setq helm-grep-ag-pipe-cmd-switches '("--color-match '31;43'"))
+    ;(setq helm-grep-ag-command "ag --line-numbers -S --hidden --color --color-match '31;43' --nogroup %s %s %s")
+    ;(setq helm-grep-ag-pipe-cmd-switches '("--color-match '31;43'"))
+    ; use Rg
+    (setq helm-grep-ag-command "rg --color=always --colors 'match:fg:black' --colors 'match:bg:yellow' --smart-case --no-heading --line-number %s %s %s")
+    (setq helm-grep-ag-pipe-cmd-switches '("--colors 'match:fg:black'" "--colors 'match:bg:yellow'"))
     
     (add-to-list 'helm-sources-using-default-as-input 'helm-source-man-pages)
 
